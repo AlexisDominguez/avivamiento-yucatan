4530dbbb9582feec1f7cff580cfe226e9da59a4a
Alexis Dominguez <alexisdominguez@Alexiss-MacBook-Air.local>
2024-08-03 11:38:13 -0600

    Improved functionality of carousel

--- src/modules/main/About.tsx
+++ src/modules/main/About.tsx
@@ -1,12 +1,12 @@
-import { MdOutlineChurch, MdOutlineWhatshot } from "react-icons/md";
-import { Button } from "../../components/Button";
-import { ROUTES } from "../../consts/routes";
-import { AiFillHeart } from "react-icons/ai";
-import { Chip } from "../../components/Chip";
-import { useState } from "react";
-import { type Sections } from "../../types/about/about";
+import { MdOutlineChurch, MdOutlineWhatshot } from 'react-icons/md';
+import { Button } from '../../components/Button';
+import { ROUTES } from '../../consts/routes';
+import { AiFillHeart } from 'react-icons/ai';
+import { Chip } from '../../components/Chip';
+import { useState } from 'react';
+import { type Sections } from '../../types/about/about';
 
-import { ABOUT_COPYWRITING } from "../../copywriting/about/ABOUT_COPYWRITING";
+import { ABOUT_COPYWRITING } from '../../copywriting/about/ABOUT_COPYWRITING';
 
 const Section = ({ section }: { section: Sections }) => {
   return (
@@ -22,7 +22,7 @@ const Section = ({ section }: { section: Sections }) => {
         {ABOUT_COPYWRITING[section].description}
       </p>
       <a href={ROUTES.about}>
-        <Button classes="mt-8" fullWidth>
+        <Button classes="mt-8" fullWidth filled>
           Leer más
         </Button>
       </a>
@@ -31,7 +31,7 @@ const Section = ({ section }: { section: Sections }) => {
 };
 
 export const About = () => {
-  const [selectedChip, setSelectedChip] = useState<Sections>("pastors");
+  const [selectedChip, setSelectedChip] = useState<Sections>('pastors');
 
   const handleSelectedChip = (value?: string) => {
     if (value) setSelectedChip(value as Sections);
@@ -39,27 +39,27 @@ export const About = () => {
 
   return (
     <>
-      <h3 className="font-medium mt-4 text-xl">Nosotros</h3>
-      <div className="flex mt-4 ml-[-8px] justify-between">
+      <h3 className="font-medium mt-4 text-2xl">Nosotros</h3>
+      <div className="flex mt-3 ml-[-8px] justify-between">
         <Chip
-          label={"Pastores"}
-          selected={selectedChip === "pastors"}
+          label={'Pastores'}
+          selected={selectedChip === 'pastors'}
           value="pastors"
           Icon={<AiFillHeart size={20} />}
           onClick={handleSelectedChip}
         />
         <Chip
           Icon={<MdOutlineChurch size={20} />}
-          selected={selectedChip === "church"}
-          label={"Iglesia"}
+          selected={selectedChip === 'church'}
+          label={'Iglesia'}
           value="church"
           onClick={handleSelectedChip}
         />
         <Chip
           Icon={<MdOutlineWhatshot size={20} />}
-          label={"Creencias"}
+          label={'Creencias'}
           value="beliefs"
-          selected={selectedChip === "beliefs"}
+          selected={selectedChip === 'beliefs'}
           onClick={handleSelectedChip}
         />
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,33 @@
-import defaultTheme from "tailwindcss/defaultTheme";
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       animation: {
-        "fade-in": "fade-in 1s",
+        'fade-in': 'fade-in 1s'
       },
       keyframes: {
-        "fade-in": {
-          "0%": {
-            opacity: "0",
+        'fade-in': {
+          '0%': {
+            opacity: '0'
           },
-          "100%": {
-            opacity: "1",
-          },
-        },
+          '100%': {
+            opacity: '1'
+          }
+        }
       },
+      colors: {
+        ...colors,
+        'purple-text': '#AB9EC7',
+        'purple-background': '#F5F0FF'
+      }
     },
     fontFamily: {
-      sans: ["OpenSauceSans", ...defaultTheme.fontFamily.sans],
-    },
+      sans: ['OpenSauceSans', ...defaultTheme.fontFamily.sans]
+    }
   },
-  plugins: [],
+  plugins: []
 };
